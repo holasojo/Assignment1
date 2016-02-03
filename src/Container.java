a22282f5c39f647fa1f33c88eafff21a28f6cee7
holasojo <joso930115@gmail.com>
2016-02-03 10:10:57 -0500

    File /Users/SOJO/Documents/eclipse_workspace/Assignment1/src/Container.java changed at Wed Feb 03 10:10:57 EST 2016

--- src/Container.java
+++ src/Container.java
@@ -37,17 +37,13 @@ public class Container {
     public void insert(String name, int x, int y, int w, int h) {
         if (fits(x, y, w, h)) {
             RectangleValue rec = new RectangleValue(x, y, w, h);
-            KVPair<String, RectangleValue> kv = new KVPair<String, 
-                    RectangleValue>(
-                    name, rec);
+            KVPair<String, RectangleValue> kv = new KVPair<String, RectangleValue>(name, rec);
             // insert kv into the list
             list.insert(kv);
-            System.out.println("Rectangle inserted: (" + name + ", " + x
-                    + ", " + y + ", " + w + ", " + h + ")");
+            System.out.println("Rectangle inserted: (" + name + ", " + x + ", " + y + ", " + w + ", " + h + ")");
         }
         else {
-            System.out.println("Rectangle rejected: (" + name + ", " + x
-                    + ", " + y + ", " + w + ", " + h + ")");
+            System.out.println("Rectangle rejected: (" + name + ", " + x + ", " + y + ", " + w + ", " + h + ")");
         }
     }
 
@@ -66,8 +62,7 @@ public class Container {
      */
     public boolean fits(int x, int y, int w, int h) {
 
-        return (w > 0 && h > 0) && (x >= 0 && y >= 0)
-                && ((x + w <= 1024) && (y + h <= 1024));
+        return (w > 0 && h > 0) && (x >= 0 && y >= 0) && ((x + w <= 1024) && (y + h <= 1024));
 
     }
 
@@ -79,16 +74,14 @@ public class Container {
      */
     public void remove(String name) {
 
-        KVPair<String, RectangleValue> toRemove = new KVPair<String, 
-                RectangleValue>(
-                name, new RectangleValue(1, 1, 1, 1));
+        KVPair<String, RectangleValue> toRemove = new KVPair<String, RectangleValue>(name,
+                new RectangleValue(1, 1, 1, 1));
 
         KVPair<String, RectangleValue> removed = list.remove(toRemove);
 
         if (removed != null) {
 
-            System.out.println("Rectangle removed: " + "("
-                    + removed.toString() + ")");
+            System.out.println("Rectangle removed: " + "(" + removed.toString() + ")");
         }
         else {
 
@@ -114,18 +107,15 @@ public class Container {
             RectangleValue rec = new RectangleValue(x, y, w, h);
             KVPair<String, RectangleValue> removed = list.remove(rec);
             if (removed != null) {
-                System.out.println("Rectangle removed: " + "("
-                        + removed.toString() + ")");
+                System.out.println("Rectangle removed: " + "(" + removed.toString() + ")");
             }
             else {
 
-                System.out.println("Rectangle not removed: "
-                        + rec.toString());
+                System.out.println("Rectangle not removed: " + rec.toString());
             }
         }
         else {
-            System.out.println("Rectangle rejected: (" + x + ", " + y
-                    + ", " + w + ", " + h + ")");
+            System.out.println("Rectangle rejected: (" + x + ", " + y + ", " + w + ", " + h + ")");
         }
 
     }
@@ -145,14 +135,16 @@ public class Container {
      * @return false when the rectangle is not within the region.
      */
     public boolean regionSearch(int x, int y, int w, int h) {
-        if (w > 0 && h > 0) {
-            list.regionsearch(new RectangleValue(x, y, w, h));
-            return true;
+        if (w > 0) {
+            if (h > 0) {
+                list.regionsearch(new RectangleValue(x, y, w, h));
+                return true;
+            }
+            return false;
         }
         else {
 
-            System.out.println("Rectangle rejected: (" + x + ", " + y
-                    + ", " + w + ", " + h + ")");
+            System.out.println("Rectangle rejected: (" + x + ", " + y + ", " + w + ", " + h + ")");
             return false;
         }
 
