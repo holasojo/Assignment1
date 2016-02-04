90291d53f8afff2bc0e76d86072d0a3af6a74f55
holasojo <joso930115@gmail.com>
2016-02-04 11:09:04 -0500

    File /Users/SOJO/Documents/eclipse_workspace/Assignment1/src/Container.java changed at Thu Feb 04 11:09:04 EST 2016

--- src/Container.java
+++ src/Container.java
@@ -143,7 +143,7 @@ public class Container {
      * @return false when the rectangle is not within the region.
      */
     public boolean regionSearch(int x, int y, int w, int h) {
-        if (w > 0) {
+        if (w > 0 && h > 0) {
             list.regionsearch(new RectangleValue(x, y, w, h));
             return true;
 
