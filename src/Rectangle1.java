dbabebfd5671b21d4a6b98704b68d98ef645704c
holasojo <joso930115@gmail.com>
2016-02-04 14:50:20 -0500

    File /Users/SOJO/Documents/eclipse_workspace/Assignment1/src/Rectangle1.java changed at Thu Feb 04 14:50:20 EST 2016

--- src/Rectangle1.java
+++ src/Rectangle1.java
@@ -3,6 +3,26 @@
  * Project 1 for CS 3114 Spring 2016 The class containing the main method, the
  * entry point of the application.
  *
+ * On my honor:
+//
+// - I have not used source code obtained from another student,
+//   or any other unauthorized source, either modified or
+//   unmodified.
+//
+// - All source code and documentation used in my program is
+//   either my original work, or was derived by me from the
+//   source code published in the textbook for this course.
+//
+// - I have not discussed coding details about this project with
+//   anyone other than my partner (in the case of a joint
+//   submission), instructor, ACM/UPE tutors or the TAs assigned
+//   to this course. I understand that I may discuss the concepts
+//   of this program with other students, and that another student
+//   may help me debug my program so long as neither of us writes
+//   anything during the discussion or modifies any computer file
+//   during the discussion. I have violated neither the spirit nor
+//   letter of this restriction.
+ * 
  * @author So Hyun Jo (sohyun)
  * @author Scott Shumway (sshumway)
  * @version 1/23/16.
